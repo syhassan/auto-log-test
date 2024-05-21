d0b84cf2301bbeba2d92a04449b2b494a29fdfd7
Roshaan Hassan <rosh.hass@gmail.com>
2024-05-21 14:36:49 +0200

    auto commit

--- src/logging_test2.cpp
+++ src/logging_test2.cpp
@@ -12,11 +12,11 @@ void CbLogger2::run()
   ros::Rate rate(0.6);
   while(ros::ok())
   {
-    ROS_DEBUG_STREAM("2");
-    ROS_INFO_STREAM("2");
-    ROS_WARN_STREAM("2");
-    ROS_ERROR_STREAM("2");
-    ROS_FATAL_STREAM("2");
+    ROS_DEBUG_STREAM("3");
+    ROS_INFO_STREAM("3");
+    ROS_WARN_STREAM("3");
+    ROS_ERROR_STREAM("3");
+    ROS_FATAL_STREAM("3");
     rate.sleep();
   }
 }
