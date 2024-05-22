3e977aded5e6a3a7178fc2ea86fa78433685befb
Roshaan Hassan <rosh.hass@gmail.com>
2024-05-22 10:47:29 +0200

    demo

--- logger.sh
+++ logger.sh
@@ -17,7 +17,7 @@ while :
     time_diff=$((current - event_time))
     
     if [[ "$time_diff" -gt "$timeout" && "$flag" = true ]]; then
-      git -C /home/rosh/catkin_ws/src/auto-log-test add . && git -C /home/rosh/catkin_ws/src/auto-log-test commit -m 'auto commit' && git -C /home/rosh/catkin_ws/src/auto-log-test push origin master
+      git -C /home/rosh/catkin_ws/src/auto-log-test add . && git -C /home/rosh/catkin_ws/src/auto-log-test commit -m 'demo' && git -C /home/rosh/catkin_ws/src/auto-log-test push origin master
       flag=false
     fi
 
